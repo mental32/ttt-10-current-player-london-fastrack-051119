2267054f087f396c998f7ce3433eca596556f68f
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-11 13:38:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,4 +6,4 @@ def current_player(board)
   (turn_count(board) % 2 == 0) ? "X" : "O"
 end
 
-turn_count(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
+puts turn_count(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
